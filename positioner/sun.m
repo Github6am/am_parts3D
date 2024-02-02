2e76a019937ec8bd3ac4039fc31b1f3be7890eb8
Andreas Merz <opti@dada.franken.de>
2024-02-02 01:41:55 +0100

    severe rewrite of suntracker

--- positioner/sun.m
+++ positioner/sun.m
@@ -1,26 +1,52 @@
-function [az, el] = sun(t, lon);   
-% function [az, el] = sun(t, lon); 
-% report the position of the sun above the equator in radians
+function azel = sun(t, latlon);   
+% function azel = sun(t, lon); 
+% report the position of the sun above the equator in deg
 %
-% t   - is in days starting 01-01-0000, as provided by now();
-% lon - longitude of your location in deg
-% 
-% % this is yet a very simple model.
-% % for more sophisticated ephemeris, consider e.g.:
-% pkg load calcephoct
+% t       - is UTC time as provided by t=gmtime(time());
+% latlon  - geographic latitude and longitude of your location in deg
+% azel    - azimuth and elevation of the sun as a 2D-vector in radians
+%
+% Usage examples:
+%   t=time();
+%   gmt=gmtime(t);
+%   t0=t-(3600*gmt.hour+60*gmt.min+gmt.sec) +(111 - gmt.yday)*24*3600; % equinox
+%   sun(gmtime(t0 +12*3600),[50,0])  % sun az/el at equinox noon time
+%   
+%
+% Background:
+%   https://docs.octave.org/v4.0.0/Timing-Utilities.html
+%   https://de.wikipedia.org/wiki/Stundenwinkel
+%   https://de.wikipedia.org/wiki/Sonnenstand
+%   https://de.wikipedia.org/wiki/Zeitgleichung
+%   % this is yet a very simple model.
+%
+%
 %
 
   % https://de.wikipedia.org/wiki/Ekliptik#Die_Schiefe_der_Ekliptik
   eps=23.44;
-  if(~exist('lon','var'))
-    lon=11;
+  if(~exist('latlon','var'))
+    latlon=[49.6, 11.0];     % somewhere in Bavaria
   end
-  lon=lon/360;
-  tv = datevec(t);
-  s  = tv*[0 0 0 3600 60 1]';         % seconds of day since midnight
-  az = (s/(24*3600)+lon-0.5)*2*pi;    % azimuth angle, 12h entspricht 0deg
-  if(az<0) az=az+pi; end;             % return azimut values [0 ..2pi]
-  el = cos(az)*eps*pi/180;            % elevation angle over equatorial plane
+  if(~exist('t','var'))
+    t=gmtime(time());
+  end
+  lon=latlon(2)/360;
+  td  = t.sec +60*t.min +3600*t.hour;        % seconds of day since midnight
+  %th=td/3600
+  aze = (td/(24*3600)+lon-0.5)*2*pi;         % azimuth angle, 12h corresponds to 0deg on eq plane
+  ty  = (td/(24*3600) + t.yday -111)/365.25; % relative time of year, ty=0 -> spring equinox
+  ele = sin(ty*2*pi)*eps*pi/180;             % elevation angle over equatorial plane
+  
+  ee=[cos(aze)*cos(ele); sin(aze)*cos(ele); sin(ele)]; % unit pointing direction vector
+  %ee'*ee  % check if unit vector
+  phi=(90-latlon(1))/180*pi;    % polar angle of our geolocation
+  s=sin(phi);
+  c=cos(phi);
+  Ry=[c 0 -s ; 0 1 0 ; s 0 c];  % Rotation matrix when moving from north pole along the greenwich meridian
+  eh=Ry*ee;                     % transform coordinate system to our latitude
+  % Projection to earth surface and its normal
+  azel=180/pi*[atan2(eh(2),eh(1)) atan2(eh(3),sqrt(eh(1)^2+eh(2)^2))];
   return;
 end
 
--- positioner/suntracker.m
+++ positioner/suntracker.m
@@ -9,11 +9,14 @@
 % GNU octave script.
 %
 
-  v=360;       % positioner speed
-  cue=1;       % realtime factor
-  urs=30;      % positioner update rate in s
-  abias=30;    % az bias angle to avoid negative positioner angles.
-  ebias=90;    % el bias angle to avoid negative positioner angles.
+  % settings
+  v=1200;                 % positioner speed
+  cue=1;                  % cue>1: simulation mode: time step in s between updates
+  t0=6*3600 +77*24*3600;  % start time in simulation mode: 6:00 at eqinox
+  urs=30;                 % positioner update rate in s
+  abias=90;               % az bias angle to avoid negative positioner angles.
+  ebias=90;               % el bias angle to avoid negative positioner angles.
+  tfmt="%Y-%m-%d %T";
   if(~exist('track','var'))
     track=0.5;   % if set to 0.5, the mirror image shall stay at the same position.
   end
@@ -35,22 +38,32 @@
   printf( 'adjust the positioner manually to the desired pointing direction\n');
   printf( '.. waiting 20 sec ..\n');
   pause(20);
-
-  t0=now;
-  [az0,el0]=sun(t0);   % remember current start position
+  if cue<=1
+    t0=time();
+  end
+  azel0=sun(gmtime(t0));   % remember current start position
   t2=t0;
   for i = 1:1200
-    while(t2 < (i*urs/(24*3600)+t0))  % poll system time
-      pause(0.5);
-      t2=(now-t0)*cue+t0;
+    if cue==1   % realtime mode
+      while(t2 < (i*urs+t0))  % poll system time
+        pause(0.2);
+        t2=time();
+      end
+    else        % simulation mode
+      t2=t0+(i-1)*cue;
+      pause(urs);
     end
-    [az,el]=sun(t2);              % current position
-    x=(az-az0)*track*180/pi;
-    y=(el-el0)*track*180/pi;
-    msg=sprintf('G1 F%d X%d Y%d\n', v, abias+x, ebias+y);
-    printf( '%4d  %s  [%6.2f %6.2f]   %s', i, datestr(t2,31), x, y, msg);
+    gmt=gmtime(t2);
+    azel=sun(gmt);              % current position
+    d=(azel-azel0)*track;
+    x=d(1)+abias;
+    y=d(2)+ebias;
+    x=mod(x,360);
+    msg=sprintf('G1 F%d X%d Y%d\n', v, x, y);
+    printf( '%4d  %s [%6.2f %6.2f] -> [%6.2f %6.2f]   %s', i, strftime(tfmt,gmt), azel(1),azel(2), x, y, msg);
     n = srl_write( com, msg);
     pause(urs*0.8/cue);
   end
+
 
   srl_close(com);
